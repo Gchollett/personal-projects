e00031a93df6df0cec41392e1eab74d245aae21d
garrett <garrettchollett@yahoo.com>
2022-02-19 18:48:57 -0600

    Worked on Token and Battliefield Classes

--- src/main/scala/tcgTokenCompanion/BattleField.scala
+++ src/main/scala/tcgTokenCompanion/BattleField.scala
@@ -1,9 +1,21 @@
 package tcgTokenCompanion
 
 class BattleField(){
-    private var battleField = Nil
-
-    def display():Unit = {
-        
+    private var battleField:List[Token] = Nil
+    
+    def add(tkn:Token):Unit = this.battleField :+= tkn
+    def remove(tkn:Token):Unit = {
+        var temp:List[Token] = Nil
+        if(this.battleField.contains(tkn)){
+            for(card <- this.battleField){
+                if(card != tkn) temp :+= card
+            }
+        }
+        this.battleField = temp
     }
+}
+
+object BattleField {
+    def apply() = new BattleField()
+    
 }
