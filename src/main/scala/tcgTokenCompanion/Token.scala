9acb4a9d577f657d2cdf48a29d678148f1de328d
garrett <garrettchollett@yahoo.com>
2022-02-19 18:49:04 -0600

    stuff

--- src/main/scala/tcgTokenCompanion/Token.scala
+++ src/main/scala/tcgTokenCompanion/Token.scala
@@ -3,5 +3,12 @@ package tcgTokenCompanion
 import javax.swing.text.html.ImageView
 
 class Token(val name:String, var types:List[String], var power:Char, var touphness:Char, var colors:String, val image:ImageView) {
+    var number = 0
+    var tapState = false
+    def add():Unit = this.number += 1
+    def remove():Unit = this.number -= 1
+    def tap():Unit = this.tapState == !tapState
+}
+object Token{
     
 }
