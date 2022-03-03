17fd314d27930c12378544c171314c08cd39bae0
garrett <garrettchollett@yahoo.com>
2022-03-02 23:05:19 -0600

    Trying to make the stage update easily

--- src/main/scala/tcgTokenCompanion/Main.scala
+++ src/main/scala/tcgTokenCompanion/Main.scala
@@ -7,18 +7,32 @@ import scalafx.scene.layout._
 import scalafx.scene.input.MouseEvent
 import scalafx.scene.image.Image
 import scalafx.scene.control.ComboBox
+import javafx.animation.AnimationTimer
+import scala.util.control.Breaks._
 
 object Main extends JFXApp {
     val field = BattleField()
-    field.add(Token("Angel", List("Angel"), 4, 4, "w", new Image("https://c1.scryfall.com/file/scryfall-cards/large/front/3/2/323dd323-2ca2-4c85-9ef6-34341cd40d96.jpg?1641306228")))
+    val dataBase = BattleField()
+    dataBase.add(Token("Angel", List("Angel"), 4, 4, "w", new Image("https://c1.scryfall.com/file/scryfall-cards/large/front/3/2/323dd323-2ca2-4c85-9ef6-34341cd40d96.jpg?1641306228")))
     var widthOfStage = 0
     stage = new JFXApp.PrimaryStage {
         title = "MTG Token Companion"
         scene = new Scene(600, 700) {
                 val vbox = new VBox
-                val grid = createBoardState(field)
+                var grid = createBoardState(field)
                 val list = new ComboBox(createList())
-                content = grid
+                vbox.children = List(list,grid)
+                list.onAction = () => {
+                    for(x <- dataBase.battleField()){
+                        if(x.name == list.value.value){
+                            field.add(x)
+                            grid = createBoardState(field)
+                            vbox.children = List(list,grid)
+                            content = vbox
+                        }
+                    }
+                }
+                content = vbox
                 widthOfStage = width.value.toInt
         }
     }
@@ -31,9 +45,10 @@ object Main extends JFXApp {
             x.image.setFitWidth(Token.WIDTH)
             x.image.preserveRatio() = true
             x.image.onMouseClicked = (e:MouseEvent) => {
-                if(e.getButton().toString == "PIMARY"){
+                if(e.getButton().toString == "PRIMARY"){
                     x.tap
                     tapCard(x)
+                    println("tapped")
                 } else bf.remove(x)
             }
             grid.add(x.image, xGrid, yGrid)
@@ -48,5 +63,5 @@ object Main extends JFXApp {
 
     def tapCard(tkn:Token) = if(tkn.tapState) tkn.image.setRotate(90) else tkn.image.setRotate(0)
 
-    def createList() = List("Angle","Dog","Shrine")
+    def createList() = List("Angel","Dog","Shrine")
 }
